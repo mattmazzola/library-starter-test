3c62123de62ec95143e66f1a4687d78c1d9ae834
Matt Mazzola <mattm@microsoft.com>
2018-01-31 11:45:04 -0800

    New typescript library starter install with manual TypeScript 2.6.2 for lodash .d.ts issue

---
+++ test/library-starter-test.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from '../src/library-starter-test'
+
+/**
+ * Dummy test
+ */
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy()
+  })
+
+  it('DummyClass is instantiable', () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
