8b76f51a8dea218c5952d5633b686396dd87b086
Matt Mazzola <mattm@microsoft.com>
2018-01-31 22:24:08 +0000

    docs(docs): update gh-pages

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+﻿var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"128":"Class"},"rows":[{"id":0,"kind":128,"name":"DummyClass","url":"classes/dummyclass.html","classes":"tsd-kind-class"},{"id":1,"kind":128,"name":"OtherDummyClass","url":"classes/otherdummyclass.html","classes":"tsd-kind-class"},{"id":2,"kind":128,"name":"ThirdDummyClass","url":"classes/thirddummyclass.html","classes":"tsd-kind-class"}]};
